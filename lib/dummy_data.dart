de4cc0943240a350f89248953acd568aa7fb03dc
DiseirangCHHEUN <111745190+Diseirang@users.noreply.github.com>
2023-01-05 18:02:18 +0700

    Change

---
+++ lib/dummy_data.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '/models/category.dart';
+
+// ignore: unnecessary_const
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Harmburgars',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'German',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Italian',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Cambodia',
+    color: Colors.white,
+  ),
+];
